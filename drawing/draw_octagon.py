fff0f4b35350ccbfbfeab4d8acc3b687191d0145
Yuko Muranushi <Yukorin5@users.noreply.github.com>
2015-04-19 03:10:38 +0900

    Update draw_octagon.py

--- drawing/draw_octagon.py
+++ drawing/draw_octagon.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+!/usr/bin/env python
+ -*- coding: utf-8 -*-  #Change encoding.
 
 from turtle import *
 
 for i in range(8):
  forward(100)
- right(45) #left(45)でも可
- #何もしないでEnterキーで終了
+ right(45)     #left(45)でも可
+
+#何もしないでEnterキーで終了
