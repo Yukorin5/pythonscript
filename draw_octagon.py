4b4b3e33e5db6c756a91a78ba48a262e34506c7a
Yukorin5 <hada40y@gmail.com>
2015-12-06 15:52:48 +0900

    add input()

--- draw_octagon.py
+++ draw_octagon.py
@@ -6,3 +6,4 @@ from turtle import *
 for i in range(8):
  forward(100)
  right(45) #left(45)でも可
+input()
--- drawing/draw_hexagon.py
+++ drawing/draw_hexagon.py
@@ -5,5 +5,6 @@ from turtle import *
 for i in range(6):
  forward(100)
  right(60)
- 
+
  #何もしないでEnterキーを押して終了
+input()
