822bf900f64fc55fc32c32b380aa17de23473793
Takayuki Muranushi <muranushi@gmail.com>
2016-01-03 15:09:29 +0900

    add a script to analyze aia and plot histograms.

---
+++ analyze-aia/animate-histogram.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import math, random, os, sys, re, subprocess
+import matplotlib as mpl
+mpl.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+workdir = sys.argv[1]
+
+# list all files in the workdir.
+allfns = os.listdir(workdir)
+
+fns = []
+
+# append filenames to list variable fns,
+for fn in allfns:
+    # for only fn that ends with '.fits' .
+    if re.search('\.fits$', fn):
+        fns.append(workdir + '/' +fn)
+
+# sort the files alphabetically.
+fns =  sorted(fns)
+
+for fn in fns:
+    pngfn = re.sub('\.fits$', '.png', fn)
+    print "plotting histogram: ", fn , '->' , pngfn
+
+    h = fits.open(fn)
+    h[1].verify('fix')
+    exptime = h[1].header['EXPTIME']
+    if exptime <=0:
+        print "Warning: non-positive EXPTIME: ", h[1].header['EXPTIME']
+        exptime = 2.0
+
+    # adjust the pixel luminosity with the exposure time.
+    img = h[1].data / exptime
+    fig,ax = plt.subplots()
+
+    one_frame = plt.hist(img.flat, range(0,20000,200), log = True, color='blue')
+    ax.set_title(fn)
+    plt.savefig(pngfn, dpi=100)
+    plt.close('all')
+
+print "creating animation."
+subprocess.call('convert -loop 1 -delay 20 {}/*.png aia.gif'.format(workdir),shell = True)
