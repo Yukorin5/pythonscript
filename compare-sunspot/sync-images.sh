8fa230b93088f1a42ceb48b7dc54a0f0921db369
Takayuki Muranushi <muranushi@gmail.com>
2016-08-14 11:21:35 +0900

    Image syncer

---
+++ compare-sunspot/sync-images.sh
@@ -0,0 +1,4 @@
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-hmi/*.png b-hmi/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-hmi/*.png d-hmi/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-aia/*.png b-aia/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-aia/*.png d-aia/
