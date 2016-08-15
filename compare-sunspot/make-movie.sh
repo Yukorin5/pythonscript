7deb21758b5dac893c8dd75e258949a63e97542a
Takayuki Muranushi <muranushi@gmail.com>
2016-08-15 21:54:15 +0900

    making movie for 6 pages

--- compare-sunspot/make-movie.sh
+++ compare-sunspot/make-movie.sh
@@ -1,1 +1,6 @@
-ffmpeg -r 24 -i f-720-test/%08d.png -qscale 0 output.mp4
+ffmpeg -r 24 -i f-aia193-12135-720/%08d.png -qscale 0 m-ar-aia193-12135-720.mp4
+ffmpeg -r 24 -i f-aia193-12297-720/%08d.png -qscale 0 m-ar-aia193-12297-720.mp4
+ffmpeg -r 24 -i f-aia94-12135-720/%08d.png  -qscale 0 m-ar-aia94-12135-720.mp4
+ffmpeg -r 24 -i f-aia94-12297-720/%08d.png  -qscale 0 m-ar-aia94-12297-720.mp4
+ffmpeg -r 24 -i f-hmi-12135-720/%08d.png    -qscale 0 m-ar-hmi-12135-720.mp4
+ffmpeg -r 24 -i f-hmi-12297-720/%08d.png    -qscale 0 m-ar-hmi-12297-720.mp4
