bd165ac225b921de968144fb0b4ce588c7239128
Takayuki Muranushi <muranushi@gmail.com>
2016-08-15 17:44:15 +0900

    Frame plotter

---
+++ compare-sunspot/create_frames.sh
@@ -0,0 +1,7 @@
+./plot-AR-frames.py hmi    12135 86400 &
+./plot-AR-frames.py aia94  12135 86400 &
+./plot-AR-frames.py aia193 12135 86400 &
+
+./plot-AR-frames.py hmi    12297 86400 &
+./plot-AR-frames.py aia94  12297 86400 &
+./plot-AR-frames.py aia193 12297 86400 &
