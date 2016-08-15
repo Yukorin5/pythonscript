18ce1c1d190ff88b581db60acbda05dd6c390c55
Takayuki Muranushi <muranushi@gmail.com>
2016-08-15 17:56:59 +0900

    Syncer

--- compare-sunspot/sync-images.sh
+++ compare-sunspot/sync-images.sh
@@ -1,4 +1,11 @@
-rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-hmi/*.png b-hmi/
-rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-hmi/*.png d-hmi/
-rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-aia/*.png b-aia/
-rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-aia/*.png d-aia/
+#rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-hmi/*.png b-hmi/
+#rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-hmi/*.png d-hmi/
+#rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/b-aia/*.png b-aia/
+#rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/d-aia/*.png d-aia/
+
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-aia193-12135-86400/ f-aia193-12135-86400/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-aia193-12297-86400/ f-aia193-12297-86400/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-aia94-12135-86400/  f-aia94-12135-86400/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-aia94-12297-86400/  f-aia94-12297-86400/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-hmi-12135-86400/    f-hmi-12135-86400/
+rsync -avz nushio16k-out:/home/nushio/hub/Yukorin5/pythonscript/compare-sunspot/f-hmi-12297-86400/    f-hmi-12297-86400/
