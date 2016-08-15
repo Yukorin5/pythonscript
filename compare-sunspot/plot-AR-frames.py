3cc2e5aa4f23664a26a05b4f32688d3fd87378a3
Takayuki Muranushi <muranushi@gmail.com>
2016-08-15 17:39:44 +0900

    AR plotter written

--- compare-sunspot/plot-AR-frames.py
+++ compare-sunspot/plot-AR-frames.py
@@ -18,16 +18,21 @@ img_type = sys.argv[1]
 ar_no = int(sys.argv[2])
 cadence = datetime.timedelta(seconds = int(sys.argv[3]))
 
+out_path = "f-{}-{}-{}/".format(img_type, ar_no, cadence.total_seconds())
+subprocess.call(["mkdir", "-p", out_path])
+
+
 omega = 2 * math.pi  / 28 / 86400
 
 if ar_no == 12135:
     time_begin=datetime.datetime(2014,8,5)
-    time_end=datetime.datetime(2014,8,5)
+    time_end=datetime.datetime(2014,8,19)
     Ax = 900
     Ay = 100
     x0 = 0
     y0 = 200
     t0 = datetime.datetime(2014,8,11,15,46)
+    data_path_head = "b-"
 elif ar_no == 12297:
     time_begin=datetime.datetime(2015,3,4)
     time_end=datetime.datetime(2015,3,21)
@@ -36,6 +41,7 @@ elif ar_no == 12297:
     x0 = 0
     y0 = -300
     t0 = datetime.datetime(2015,3,13,1,22)
+    data_path_head = "d-"
 else:
     print "unknown AR #", ar_no
     exit(1)
@@ -48,30 +54,33 @@ while True:
     if t >= time_end:
         exit(0)
 
-
-    fnbody,ext = os.path.splitext(fn)
-
-    if '193.image' in fn:
-        img_type = 'aia193'
-    elif '94.image' in fn:
-        img_type = 'aia94'
-    elif 'hmi' in fn:
-        img_type = 'hmi'
+    if img_type == "hmi":
+        fn ="{}hmi/hmi.M_720s.{}_TAI.6173.0.magnetogram.fits".format(
+            data_path_head, t.strftime("%Y%m%d_%H%M%S"))
+    elif img_type == "aia94":
+        fn = "{}aia/aia.lev1_euv_12s.{}03Z.94.image_lev1.fits".format(
+            data_path_head, t.strftime("%Y-%m-%dT%H%M"))
+    elif img_type == "aia193":
+        fn = "{}aia/aia.lev1_euv_12s.{}08Z.193.image_lev1.fits".format(
+            data_path_head, t.strftime("%Y-%m-%dT%H%M"))
     else:
-        print "Unknown image type:" , fn
+        print "unknown image type: ", fn
         exit(0)
+
+
 
 
     fullmap = sunpy.map.Map(fn)
 
     length = 250 * u.arcsec
-    x0 = -700 * u.arcsec
-    y0 = -250 * u.arcsec
+    t_i = (t-t0).total_seconds()
+    x = u.arcsec * Ax * math.cos(omega * t_i + 1.5*math.pi) + x0
+    y = u.arcsec * Ay * math.cos(omega * t_i + 1.5*math.pi) + y0
 
     # Create a SunPy Map, and a second submap over the region of interest.
 
-    img = fullmap.submap(u.Quantity([x0 - length, x0 + length]),
-                         u.Quantity([y0 - length, y0 + length]))
+    img = fullmap.submap(u.Quantity([x - length, x + length]),
+                         u.Quantity([y - length, y + length]))
 
 
     print img.data.shape
@@ -106,5 +115,7 @@ while True:
     elif img_type=='aia94':
         plt.clim(0,100)
 
-    plt.savefig(img_type + '.png')
+    out_fn = out_path + "/{:08}.png".format(frame_ctr)
+
+    plt.savefig(out_fn)
     plt.close('all')
