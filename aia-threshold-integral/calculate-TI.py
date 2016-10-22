ca03dca6af5bdab825e8a63c965125feb7f1242a
Takayuki Muranushi <muranushi@gmail.com>
2016-10-22 17:33:49 +0900

    Ready to preprocess the threshold integral

--- aia-threshold-integral/calculate-TI.py
+++ aia-threshold-integral/calculate-TI.py
@@ -12,48 +12,52 @@ from astropy import units as u
 
 from observational_data import *
 
-#img = get_aia_image(193, datetime.datetime(2014,12,20,00,36))
-
-
-subprocess.call(['mkdir', '-p','frames'])
-
-filenames = []
-counter = 0
-
-
-time_begin = datetime.datetime(2013,11,26,0,00)
-time_end   = datetime.datetime(2013,11,27,0,00)
-dt = datetime.timedelta(minutes=12)
-
-
-global data_path
-work_dir = data_path + time_begin.strftime('/aia0193_work/%Y/%m/%d')
-subprocess.call("mkdir -p " + work_dir, shell=True)
-work_file = work_dir + "TI.pickle"
+global no_internet
+no_internet = True
 
 
 
-t = time_begin-dt
-ret = {}
-while True:
-    t += dt
-    if t > time_end:
-        break
+# if len(sys.argv)>2:
+#     time_begin = datetime.datetime.strptime(sys.argv[1],"%Y-%m-%d")
+#     time_end   = datetime.datetime.strptime(sys.argv[2],"%Y-%m-%d")
 
-    img = get_aia_image(193, t)
-    if img is None:
-        continue
-    img.data = img.data / (img.exposure_time / u.second)
+
+
+for day_add in xrange(366*6):
     
+    time_begin = datetime.datetime(2011,1,1,0,00) + datetime.timedelta(days=day_add)
+    time_end   = time_begin + datetime.timedelta(days=1)
+        
+    print time_begin
 
-    threshold_integrals = {}
-    for threshold in xrange(0,10000,100):
-        y = np.sum(np.maximum(0, img.data - threshold))
-        threshold_integrals[threshold] = float(y)
-    print t, threshold_integrals
-
-    ret[t] = threshold_integrals
-
-print ret
-with open(work_file,"w") as fp:
-    pickle.dump(ret, fp, -1)
+    dt = datetime.timedelta(minutes=12)
+    
+    
+    global data_path
+    work_dir = data_path + time_begin.strftime('/aia0193_work/%Y/%m/%d/')
+    subprocess.call("mkdir -p " + work_dir, shell=True)
+    work_file = work_dir + "TI.pickle"
+    
+    t = time_begin-dt
+    ret = {}
+    while True:
+        t += dt
+        if t > time_end:
+            break
+    
+        img = get_aia_image(193, t)
+        if img is None:
+            continue
+        img.data = img.data / (img.exposure_time / u.second)
+        
+    
+        threshold_integrals = {}
+        for threshold in xrange(0,10000,100):
+            y = np.sum(np.maximum(0, img.data - threshold))
+            threshold_integrals[threshold] = float(y)
+        print t, threshold_integrals
+    
+        ret[t] = threshold_integrals
+    
+    with open(work_file,"w") as fp:
+        pickle.dump(ret, fp, -1)
---
+++ aia-threshold-integral/submit.sh
@@ -0,0 +1,2 @@
+t2sub -q S -W group_list=t2g-16IAS -et 3 -l walltime=24:00:00 ./job.sh 
+
