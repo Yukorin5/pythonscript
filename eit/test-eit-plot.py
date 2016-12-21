085e4aab24564711eb812ef20a863d6ed6d5e02c
Takayuki Muranushi <muranushi@gmail.com>
2016-12-21 17:05:46 +0900

    Add color bar to the plot

--- eit/test-eit-plot.py
+++ eit/test-eit-plot.py
@@ -8,9 +8,11 @@ from astropy.io import fits
 
 
 def plot_sun_image(img, filename, wavelength=193, title = ''):
-    cmap = plt.get_cmap('sdoaia{}'.format(wavelength))
+    #cmap = plt.get_cmap('sdoaia{}'.format(wavelength))
+    cmap = plt.get_cmap('sohoeit195')
     plt.title(title)
-    plt.imshow(img,cmap=cmap,origin='lower',vmin=800, vmax=3000)#,vmin=vmin, vmax=vmax)
+    cax = plt.imshow(img,cmap=cmap,origin='lower',vmin=0, vmax=3000)#,vmin=vmin, vmax=vmax)
+    plt.gcf().colorbar(cax)
     plt.savefig(filename)
     plt.close("all")
 
